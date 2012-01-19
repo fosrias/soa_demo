f7edf0866d1fea2cedfa9bfa7041c2dd6b78e736
Mark W. Foster <mfoster@ubuntu.(none)>
2012-01-18 18:43:04 -0800

    added jquery ui javascript, app specific soa_demo.js & soa_demo.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,5 +5,53 @@
 // the compiled file.
 //
 //= require jquery
+//= require jquery.ui.core
+
+//= require jquery.effects.core
+
+// the following depend on jquery.effects.core
+// take out any unused prior to production
+//if all are unused, you may also remove jquery.effects.core
+
+//= require jquery.effects.blind
+//= require jquery.effects.bounce
+//= require jquery.effects.clip
+//= require jquery.effects.drop
+//= require jquery.effects.explode
+//= require jquery.effects.fade
+//= require jquery.effects.fold
+//= require jquery.effects.highlight
+//= require jquery.effects.pulsate
+//= require jquery.effects.scale
+//= require jquery.effects.slide
+//= require jquery.effects.transfer
+
+// the following all depend on jquery.ui.core && jquery.ui.widget
+// and may have other dependencies; files are listed below in dependency order
+// dependencies are noted in the first few lines of the .js file
+// take out any unused prior to production
+
+//= require jquery.ui.widget
+
+//= require jquery.ui.accordion
+//= require jquery.ui.autocomplete
+//= require jquery.ui.button
+//= require jquery.ui.datepicker
+//= require jquery.ui.position
+//= require jquery.ui.mouse
+//= require jquery.ui.resizable
+//= require jquery.ui.draggable
+//= require jquery.ui.droppable
+//= require jquery.ui.progressbar
+//= require jquery.ui.selectable
+//= require jquery.ui.sortable
+//= require jquery.ui.tabs
+//= require jquery.ui.dialog
+
+//= require soa_demo
+
 //= require jquery_ujs
+
+//= require soa_demo
+
 //= require_tree .
---
+++ app/assets/javascripts/soa_demo.js
@@ -0,0 +1,5 @@
+$(function () {
+    var $dspNone = $('.dsp-none');
+
+    $dspNone.fadeIn(1500);
+});
