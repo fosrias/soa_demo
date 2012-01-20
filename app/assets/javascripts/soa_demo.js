739e05a537f8a8c6696b054f66d772dc7cece251
Mark W. Foster <mfoster@ubuntu.(none)>
2012-01-20 01:11:03 -0800

    added some interactions and classes to sidebar and application

--- app/assets/javascripts/soa_demo.js
+++ app/assets/javascripts/soa_demo.js
@@ -2,8 +2,10 @@ $(function () {
     var $dspNone = $('.dsp-none'),
         $sidebar = $('#sidebar').resizable({ handles: 'e',
                                              minWidth: 150
-                                            });
-//    $sidebar.height($(window).height() - $sidebar[0].offsetTop);
-
-    $dspNone.fadeIn('fast').removeClass('dsp-none');;
+                                            }),
+        $sideBarContent =  $('#sidebar-content'),
+        sideHeight = $(window).height() - $sidebar[0].offsetTop;
+    $sidebar.height(sideHeight + 'px');
+    $sideBarContent.height((sideHeight - 52) + 'px');
+//    $dspNone.fadeIn('fast').removeClass('dsp-none');;
 });
