7b35b3f0ddce19bd866d1fb640442808499cf4ba
Mark W. Foster <mfoster@ubuntu.(none)>
2012-01-20 14:41:56 -0800

    added detail pane to SOA index page

--- app/assets/javascripts/soa_demo.js
+++ app/assets/javascripts/soa_demo.js
@@ -1,11 +1,19 @@
 $(function () {
-    var $dspNone = $('.dsp-none'),
-        $sidebar = $('#sidebar').resizable({ handles: 'e',
-                                             minWidth: 150
+    var $sidebar = $('#sidebar').resizable({ minWidth: 150
                                             }),
         $sideBarContent =  $('#sidebar-content'),
-        sideHeight = $(window).height() - $sidebar[0].offsetTop;
-    $sidebar.height(sideHeight + 'px');
-    $sideBarContent.height((sideHeight - 52) + 'px');
-//    $dspNone.fadeIn('fast').removeClass('dsp-none');;
+
+        $window = $(window),
+
+        resizeSideBar = function() {
+            var  sideHeight = $(window).height() - $sidebar[0].offsetTop;
+            $sidebar.height(sideHeight + 'px');
+            $sideBarContent.height((sideHeight - 52) + 'px');
+        };
+
+     resizeSideBar();
+
+    $window.resize( function () {
+        resizeSideBar();
+    });
 });
